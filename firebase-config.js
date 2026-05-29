c0b0fb07f6600a437d999691c2eb776ffeca09ab
GST1788 <gsteip17@gmail.com>
2026-05-30 01:33:25 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA97Kcc-tgpc_qjTgVHWAm3Aw43Xn_9Wlw",
+  authDomain: "la-seconde.firebaseapp.com",
+  databaseURL: "https://la-seconde-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "la-seconde",
+  storageBucket: "la-seconde.firebasestorage.app",
+  messagingSenderId: "508087909336",
+  appId: "1:508087909336:web:e09e94313c62c2391590ad"
+};
